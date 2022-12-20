e86cf2929a39db64222924234828cb178830bb09
Vincy-fivestones <vincy.c@fivestones.net>
2022-12-20 15:46:04 +0800

    set up

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/srceens/HomeScreen';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import RestaurantScreen from './src/srceens/RestaurantScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'home' }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="Restaurant"
+          component={RestaurantScreen}
+          options={{ title: 'Restaurant' }}
+        ></Stack.Screen>
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
+  // return (
+  //   <SafeAreaView>
+  //     <View>
+  //       <Text>Home</Text>
+  //     </View>
+  //   </SafeAreaView>
+  // );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ sanity.js
@@ -0,0 +1,14 @@
+import sanityClient from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+import 'dotenv/config';
+const client = sanityClient({
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: "production",
+    useCdn: true,
+    apiVersion: "2021-10-21"
+})
+
+const builder = imageUrlBuilder(client)
+export const urlFor = (source) => builder.image(source);
+
+export default client
